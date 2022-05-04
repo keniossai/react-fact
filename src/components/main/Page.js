91c3ddff8fab1b65ffb5e85eb29ebe5dfc5079a4
Kenneth Ossai <kenossai@ymail.com>
2022-05-04 14:48:42 +0100

    Added new css suedo code styling

--- src/components/main/Page.js
+++ src/components/main/Page.js
@@ -4,13 +4,14 @@ const Page = () => {
   return (
     <div className='page'>
         <h1 className='title'>Fun fact about React</h1>
-        <ol className='main--fact'>
+        <ul className='main--fact'>
             <li>Was first release in 2013</li>
             <li>Originally created by jordan Walke</li>
             <li>Has well over 100k stars on github</li>
             <li>Is maintained by facebook</li>
             <li>It's a popular library</li>
-        </ol>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
     </div>
   )
 }
